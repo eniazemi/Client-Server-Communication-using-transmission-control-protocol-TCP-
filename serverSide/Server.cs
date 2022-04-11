979e11a7a6a884f6a56a3536b0107278ff52cae6
Engjëll Bunjaku <engjell.bunjaku@student.uni-pr.edu>
2022-04-11 21:19:28 +0200

    some changes in server side

--- serverSide/Server.cs
+++ serverSide/Server.cs
@@ -4,6 +4,29 @@
     {
         public static void Main(string[] args)
         {
+            public static void StartServer()
+            {
+
+                IPHostEntry host = Dns.GetHostEntry("192.168.1.101");
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11001);
+
+                try
+                {
+                    Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                    listener.Bind(localEndPoint);
+                    listener.Listen(10);
+
+                    Console.WriteLine("SERVER IS READY\nWAITING FOR CONNECTION...");
+                    Secure_canal_of_communication(listener);
+                }
+        
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
         }
     }
 }

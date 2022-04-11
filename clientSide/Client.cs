eafb46afefc61471c9db6db5be82cc9272272231
Engjëll Bunjaku <engjell.bunjaku@student.uni-pr.edu>
2022-04-11 21:22:18 +0200

    some changes in client

--- clientSide/Client.cs
+++ clientSide/Client.cs
@@ -4,6 +4,19 @@
     {
         public static void Main(string[] args)
         {
-        }
+            private static void StartClient()
+            {
+                byte[] bytes = new byte[2048];
+
+                try
+                {
+                    IPHostEntry host = Dns.GetHostEntry("192.168.1.101");
+                    IPAddress ipAddress = host.AddressList[0];
+                    IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11001);
+
+                    Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                
+
+            }
     }
 }
